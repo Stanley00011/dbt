45b61160936b2f01eb45d52fb2c5fcdd8c00f8e2
Olajide Stanley Ajao <stanleyajao123@gmail.com>
2025-05-20 23:05:47 +0000

    location update

--- models/smart_waterflow_project/mart_anomalies_with_location.sql
+++ models/smart_waterflow_project/mart_anomalies_with_location.sql
@@ -1,33 +1,28 @@
 -- mart_anomalies_with_location.sql
+with
+    anomalies as (select * from {{ ref("mart_anomalies") }}),
 
-with anomalies as (
+    locations as (
 
-    select *
-    from {{ ref('mart_anomalies') }}
+        select meter_id, latitude, longitude from {{ ref("stg_meter_readings") }}
 
-),
+    ),
 
-locations as (
+    joined as (
 
-    select meter_id, latitude, longitude
-    from {{ ref('stg_meter_readings') }}
+        select
+            a.meter_id,
+            a.hour,
+            a.anomaly_status,
+            a.avg_flow_lmin,
+            a.avg_pressure_psi,
+            l.latitude,
+            l.longitude,
+            st_geogpoint(l.longitude, l.latitude) as geo_point
+        from anomalies a
+        left join locations l on a.meter_id = l.meter_id
 
-),
+    )
 
-joined as (
-
-    select
-        a.meter_id,
-        a.hour,
-        a.anomaly_status,
-        a.avg_flow_lmin,
-        a.avg_pressure_psi,
-        l.latitude,
-        l.longitude
-    from anomalies a
-    left join locations l
-    on a.meter_id = l.meter_id
-
-)
-
-select * from joined
+select *
+from joined
