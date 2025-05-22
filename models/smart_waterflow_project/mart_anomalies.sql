1fb42e19905adf949ba1935cf6a9a7cfb9fa3ca1
Olajide Ajao <stanleyajao123@gmail.com>
2025-05-22 22:14:05 +0000

    mart layers

--- models/smart_waterflow_project/mart_anomalies.sql
+++ models/smart_waterflow_project/mart_anomalies.sql
@@ -1,40 +1,30 @@
 -- models/smart_waterflow_project/mart_anomalies.sql
+{{ config(materialized="view") }}
 
-{{ config(materialized='view') }}
+with
+    flow_data as (select * from {{ ref("int_flow_summary") }}),
 
-with flow_data as (
+    flagged as (
 
-    select *
-    from {{ ref('int_flow_summary') }}
+        select
+            meter_id,
+            hour,
+            avg_flow_lmin,
+            avg_pressure_psi,
+            case
+                when avg_flow_lmin <= 0 and avg_pressure_psi <= 40
+                then 'offline'
+                when avg_pressure_psi < 50 and avg_flow_lmin > 25
+                then 'leak'
+                when avg_pressure_psi > 80 or avg_flow_lmin > 40
+                then 'spike'
+                else 'normal'
+            end as anomaly_status
+        from flow_data
 
-),
+    ),
 
-flagged as (
-
-    select
-        meter_id,
-        hour,
-        avg_flow_lmin,
-        avg_pressure_psi,
-        case 
-            when avg_flow_lmin = 0 and avg_pressure_psi = 0 then 'offline'
-            when avg_pressure_psi < 30 and avg_flow_lmin > 20 then 'leak'
-            when avg_pressure_psi > 100 or avg_flow_lmin > 100 then 'spike'
-            else 'normal'
-        end as anomaly_status
-    from flow_data
-
-),
-
-locations as (
-
-    select
-        meter_id,
-        latitude,
-        longitude
-    from {{ ref('dim_meters') }}
-
-)
+    locations as (select meter_id, latitude, longitude from {{ ref("dim_meters") }})
 
 select
     a.meter_id,
